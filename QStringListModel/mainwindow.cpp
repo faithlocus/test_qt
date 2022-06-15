cdc2e93d9a3c4a5919b1d8c29b685253c07c1942
fl <wangqing10520@sina.com>
2022-06-15 19:01:44 +0800

    add list data

--- QStringListModel/mainwindow.cpp
+++ QStringListModel/mainwindow.cpp
@@ -6,10 +6,29 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QStringList the_list;
+    the_list << "北京"
+             << "上海"
+             << "天津"
+             << "河北";
+    model_ = new QStringListModel(this);
+    model_->setStringList(the_list);
+    ui->listView->setModel(model_);
+    ui->listView->setEditTriggers(QAbstractItemView::DoubleClicked
+                                  | QAbstractItemView::SelectedClicked);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_add_clicked()
+{
+    model_->insertRow(model_->rowCount());
+    auto index = model_->index(model_->rowCount() - 1, 0);
+    model_->setData(index, "new item", Qt::DisplayRole);
+    ui->listView->setCurrentIndex(index);
 }
 
--- QStringListModel/mainwindow.h
+++ QStringListModel/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStringListModel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +16,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_add_clicked();
+
 private:
+    QStringListModel *model_;
     Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
