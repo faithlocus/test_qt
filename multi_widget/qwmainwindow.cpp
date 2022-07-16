141840008bcb9765c4724b72a309cddf640306d7
fl <wangqing10520@sina.com>
2022-07-16 16:51:07 +0800

    tabwidget 而不是tablewidget

--- multi_widget/qwmainwindow.cpp
+++ multi_widget/qwmainwindow.cpp
@@ -1,8 +1,12 @@
 #include "qwmainwindow.h"
 #include "ui_qwmainwindow.h"
+#include <QWidget>
 
 QwMainWindow::QwMainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::QwMainWindow) {
     ui->setupUi(this);
+    ui->tabWidget->setVisible(false);
+    ui->tabWidget->clear();
+    ui->tabWidget->setTabsClosable(true);
 }
 
 QwMainWindow::~QwMainWindow() {
